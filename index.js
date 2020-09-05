c4f69de42ea1d22dc2eea240d59db8991321f301
Tushar Bhardwaj <iamtushar324@gmail.com>
2020-09-05 23:45:39 +0530

    test
    Signed-off-by: Tushar Bhardwaj <iamtushar324@gmail.com>

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const exp = require('express')
+const exp = require("express");
 const app = exp();
-const session = require('express-session')
-const {db} = require('./db/db')
-const dotenv = require('dotenv')
+const session = require("express-session");
+const { db } = require("./db/db");
+const dotenv = require("dotenv");
 dotenv.config();
-
+//teset
 app.use(
-    session({
-      secret: process.env.session_sec,
-      resave: true,
-      saveUninitialized: true,
-      cookie: { httpOnly: true }
-    })
-  );
+  session({
+    secret: process.env.session_sec,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { httpOnly: true }
+  })
+);
 
-  app.use(exp.urlencoded({extended : true}))
-  app.use(exp.json())
-  app.use("/api", require("./routes/api/index").route)
+app.use(exp.urlencoded({ extended: true }));
+app.use(exp.json());
+app.use("/api", require("./routes/api/index").route);
 
 db.sync().then(() => {
-  app.listen(6000 , () => {
-      console.log('Server Started')
-  })
-})
+  app.listen(6000, () => {
+    console.log("Server Started");
+  });
+});
